89ae0612311d0f970dc0359494bd2a22c50af592
melonNamedLove <151634498+melonNamedLove@users.noreply.github.com>
2023-12-23 23:27:24 +0900

    12/23공부
    내일 더 해

--- untitled_flutter/lib/main.dart
+++ untitled_flutter/lib/main.dart
@@ -7,16 +7,53 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key:key);
-
   @override
   Widget build(BuildContext context) {
+    final listVal=["서산시","안양시","성연면"];
+    final valselected=listVal[0];
     return MaterialApp(
         home:Scaffold(
           appBar:AppBar(
-            title: Text('야옹'),
-            backgroundColor: Colors.blue,
+            leading:DropdownButton(
+              value: valselected,
+              items: listVal.map(
+                      (e) => DropdownMenuItem(child: Text(e),value:e)
+              ).toList() ,
+              style:TextStyle(
+                fontSize:30,
+              ),
+              iconSize:40,
+              onChanged: (val){
+
+              },
+            ),
+
+
+            leadingWidth: 100,
+            actions: [
+              Container(
+                  margin:EdgeInsets.all(20),
+                  child:Icon(Icons.search)
+              ),
+              Container(
+                  margin:EdgeInsets.all(20),
+                  child:Icon(Icons.menu_open)
+              ),
+              Container(
+                  margin:EdgeInsets.all(20),
+                  child:Icon(Icons.doorbell_rounded)
+              ),
+                    ],
+            
           ),
-          body:Text('ㅎㅇㅎㅇ'),
+          body:Container(
+            width: 50,height: 50,color: Colors.blue,
+            margin: EdgeInsets.all(20),
+          ),
+
+
+
+
           bottomNavigationBar: BottomAppBar(
             height: 50,
             child: Row(
