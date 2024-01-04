f418aa5dc0cf7f5594207a8f1bd4b0ae9d29915d
melonNamedLove <151634498+melonNamedLove@users.noreply.github.com>
2024-01-04 19:06:44 +0900

    main 준비

--- untitled_flutter/lib/study_backup.dart
+++ untitled_flutter/lib/study_backup.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 
 
@@ -253,3 +254,50 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const meow());
+}
+
+class meow extends StatelessWidget {
+  const meow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar:AppBar(),
+        body: ListView(
+          children: [
+            Text('d'),
+            Text('d'),
+            Text('d'),
+            Text('d'),
+            Text('d'),
+            Text('d'),
+            Text('d'),
+            Text('d'),
+            Text('f'),
+            Text('q'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class item extends StatelessWidget {
+  const item({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        child:Text('안녕')
+    );
+  }
+}
+
+*/
