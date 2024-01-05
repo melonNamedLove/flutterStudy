34fecfacf60ac0539415d5a0ca7cd6964bb405a1
melonNamedLove <151634498+melonNamedLove@users.noreply.github.com>
2024-01-05 15:43:46 +0900

    순한맛 끝

--- untitled_flutter/lib/main.dart
+++ untitled_flutter/lib/main.dart
@@ -1,44 +1,155 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'First app',
-      theme:ThemeData(
-        primarySwatch: Colors.blue
-      ),
+      title: 'meow',
 
-      home:MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      home:Grade(),
     );
   }
 }
 
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class Grade extends StatelessWidget {
+  const Grade({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        title: Text('야호'),
-        backgroundColor:Colors.blue,
+        title: Text('melon'),
+        centerTitle: true,
+        backgroundColor:Colors.amber[700],
+        elevation: 0.0 ,
       ),
-      body:Center(
-        child: Column(
-          children: <Widget>[
-            Text('야옹'),
-            Text('야옹'),
-            Text('야옹'),
-            Text('야옹'),
-          ],
+      body:Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 40.0),
+            child: Center(
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/melon.jpeg') ,
+              radius: 60.0,
+            ),
+                    ),
+          ),
+          Padding(
+          padding:EdgeInsets.fromLTRB(30.0, 0.0, 0.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            //mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+
+              Divider(
+                height:60.0,
+                color:Colors.grey[850],
+                thickness: 0.5,
+                endIndent: 30.0,
+              ),
+              Text('Name',
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(
+                height:10.0,
+              ),
+              Text('사랑이란멜론',
+                style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(
+                height:30.0,
+              ),
+              Text('tier',
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(
+                height:10.0,
+              ),
+              Text('5:5 P2',
+                style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(
+                height:30.0,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                    width:10.0,
+                  ),
+                  Text('야옹야옹',
+                    style:TextStyle(
+                        fontSize: 16.0,
+                        letterSpacing: 1.0
+                    ),),
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                    width:10.0,
+                  ),
+                  Text('매옹매옹',
+                    style:TextStyle(
+                        fontSize: 16.0,
+                        letterSpacing: 1.0
+                    ),),
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                    width:10.0,
+                  ),
+                  Text('헤윽떼윽',
+                    style:TextStyle(
+                        fontSize: 16.0,
+                        letterSpacing: 1.0
+                    ),),
+                ],
+              ),
+
+            ],
+          ),
         ),
+          Center(
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/1.png'),
+              radius :40.0,
+              backgroundColor: Colors.amber[800],
+            ),
+          )
+        ]
       ),
+
+
     );
   }
 }
