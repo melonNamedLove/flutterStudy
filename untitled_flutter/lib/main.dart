924b307dd4abcd16592856fc03a6b19e4be93d3e
melonNamedLove <151634498+melonNamedLove@users.noreply.github.com>
2024-01-07 20:06:17 +0900

    0107 study2

--- untitled_flutter/lib/main.dart
+++ untitled_flutter/lib/main.dart
@@ -1,193 +1,93 @@
 import 'package:flutter/material.dart';
+import 'animal_page.dart';
+import 'model.dart';
 
 void main(){
-  runApp(myapp());
+  runApp(const MyApp());
 }
 
-class myapp extends StatelessWidget {
-  const myapp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home:ListViewPage(),
+      home:MyPage(),
+
     );
   }
 }
 
-class ListViewPage extends StatefulWidget {
-  const ListViewPage({super.key});
+
+class MyPage extends StatefulWidget {
+  const MyPage({super.key});
 
   @override
-  State<ListViewPage> createState() => _ListViewPageState();
+  State<MyPage> createState() => _MyPageState();
 }
 
-class _ListViewPageState extends State<ListViewPage> {
+class _MyPageState extends State<MyPage> {
 
-  var titleList = [
-    'wumpus',
-    'iu',
-    'hanyohan',
-    'joyuri',
-    '돈까쓰',
-    '멜론',
-    'nyan cat',
-    'maple',
-    'LORD VADER',
-    'DEATH STAR'
+  static List<String> animalName=[
+    'ala',
+    'rabbit',
+    'frog',
+    'dragon',
+    'cat',
+    'bee',
+    'chameleon',
+    'seal',
   ];
-  var imageList = [
-    'assets/wumpus.png',
-    'assets/iu.jpg',
-    'assets/han.jpg',
-    'assets/joyuri.jpg',
-    'assets/donkatchu.jpg',
-    'assets/melon.jpeg',
-    'assets/nyancat.png',
-    'assets/1.png',
-    'assets/vader.png',
-    'assets/star.png',
+  static List<String> animalImagePath=[
+    'assets/ala.png',
+    'assets/rabbit.png',
+    'assets/frog.png',
+    'assets/dragon.png',
+    'assets/cat.png',
+    'assets/bee.png',
+    'assets/chameleons.png',
+    'assets/seal.png',
   ];
-
-  var description = [
-    '아 오늘은 7시에 일어났는데 내일은 6시에 일어나서 출근까지 해야 한다니 아침일찍 일어나는 건 너무 어렵다..',
-    '아 오늘은 7시에 일어났는데 내일은 6시에 일어나서 출근까지 해야 한다니 아침일찍 일어나는 건 너무 어렵다..',
-    '아 오늘은 7시에 일어났는데 내일은 6시에 일어나서 출근까지 해야 한다니 아침일찍 일어나는 건 너무 어렵다..',
-    '아 오늘은 7시에 일어났는데 내일은 6시에 일어나서 출근까지 해야 한다니 아침일찍 일어나는 건 너무 어렵다..',
-    '아 오늘은 7시에 일어났는데 내일은 6시에 일어나서 출근까지 해야 한다니 아침일찍 일어나는 건 너무 어렵다..',
-    '아 오늘은 7시에 일어났는데 내일은 6시에 일어나서 출근까지 해야 한다니 아침일찍 일어나는 건 너무 어렵다..',
-    '아 오늘은 7시에 일어났는데 내일은 6시에 일어나서 출근까지 해야 한다니 아침일찍 일어나는 건 너무 어렵다..',
-    '아 오늘은 7시에 일어났는데 내일은 6시에 일어나서 출근까지 해야 한다니 아침일찍 일어나는 건 너무 어렵다..',
-    '아 오늘은 7시에 일어났는데 내일은 6시에 일어나서 출근까지 해야 한다니 아침일찍 일어나는 건 너무 어렵다..',
-    '아 오늘은 7시에 일어났는데 내일은 6시에 일어나서 출근까지 해야 한다니 아침일찍 일어나는 건 너무 어렵다..',
+  static List<String> animalLocation=[
+    '대명구름채',
+    '한양하이츠빌',
+    'sm520',
+    'e편한세상',
+    'meow',
+    'HIVE',
+    'bucheon',
+    '헤윽떼윽',
   ];
 
-  void showPopUp(context, title, image, description){
-    showDialog(
-        context : context,
-        builder: (context){
-          return Dialog(
-            child: Container(
-              width: MediaQuery.of(context).size.width*0.7,
-              height: 380,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white
-              ),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    ClipRect(
-                      child: Image.asset(
-                        image,
-                        height: 200,
-                        width: 200,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(title,
-                    style: const TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey
-                    ),),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        description,
-                        maxLines: 3,
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.grey
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                
-                    ),
-                    ElevatedButton.icon(
-                      onPressed: (){
-                        Navigator.pop(context);
-                      },
-                      icon:const Icon(Icons.close,),
-                      label: const Text('close'),
-                      
-                
-                
-                    )
-                  ],
-                ),
-              ),
-            ),
-          );
-      }
-    );
-  }
+  final List<Animal> animalData = List.generate(animalName.length, (index) => Animal(animalName[index],animalImagePath[index],animalLocation[index]));
+
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width*0.6;
     return Scaffold(
       appBar: AppBar(
-        title: Text('ListView',
-          style: TextStyle(
-            color:Colors.grey
-          ),
-        ),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
+        title: const Text('ListView'),
       ),
-      body: ListView.builder(
-        itemCount: titleList.length,
+      body:ListView.builder(
+          itemCount: animalData.length,
           itemBuilder: (context, index){
-          return GestureDetector(
-            onTap: (){
-              print(titleList[index]);
-              showPopUp(context, titleList[index], imageList[index], description[index]);
-            },
-            child: Card(
-              child:Row(
-                children: [
-                  SizedBox(
-                    width: 100,
-                    height: 100,
-                      child:Image.asset(imageList[index])
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.all(10),
-                      child:Column(
-                          children:[
-                            Text(titleList[index],
-                            style:const TextStyle(
-                              fontSize:22,
-                              fontWeight:FontWeight.bold,
-                              color:Colors.grey
-                              )
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            SizedBox(
-                              width: width,
-                              child:Text(
-                                  description[index],
-                              style:TextStyle(
-                                fontSize: 15,
-                                color: Colors.grey[500]
-                              ))
-                            )
-                          ]
-                      )),
-                ],
-              )
-            ),
-          );
+            return Card(
+              child: ListTile(
+                title: Text(
+                  animalData[index].name
+                ),
+                leading: SizedBox(
+                  height: 50,
+                  width: 50,
+                  child: Image.asset(animalData[index].imgPath),
+                ),
+                onTap: (){
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) =>AnimalPage(animal:animalData[index])));
+                  print(animalData[index].name);
+                },
+              ),
+            );
           }
-      ),
+      )
     );
   }
 }
-
